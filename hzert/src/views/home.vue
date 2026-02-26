c2bd49814d45b833858337c7ffe5be0a1d4ba9d0
hzertz0 <139808204+hzertz0@users.noreply.github.com>
2026-02-26 13:52:16 -0500

    styling

--- hzert/src/views/home.vue
+++ hzert/src/views/home.vue
@@ -1,57 +1,90 @@
 <template>
-    <head>    
+  <head>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"/>
-    </head>
-    <div>
-        <h1>Potion Brewer</h1>
-        <div class ="cauldron">
-            <h1>Cauldron</h1>
-            <img src="/cauldron.png">
-        </div>
-        <div class = "container">
-            <!--<ingredients v-for="item in list" :key="item.name":item="item"/>-->
-        </div>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <div class="page">
+    <h1>Potion Brewer</h1>
+
+    <div class="cauldron">
+      <img src="/cauldron.png" />
     </div>
+
+    <div class="recipelist">
+      <img src="/scroll.png" />
+      <div class="container">
+        <!--<ingredients v-for="item in list" :key="item.name" :item="item"/>-->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
 import ingredients from '@/components/ingredients.vue'
 const list = [
-    {name: "Bat Wings",description: "A pair of wings from a bat",image: "/batwings.png"},
-    {name: "Dragon Scales",description: "Scales harvested from a magical dragon",image: "./dragonscale.png"},
-    {name: "Jellybeans",description: "Some jellybeans from a random gas station",image: "./jellybean.png"},
+  { name: 'Bat Wings', description: 'A pair of wings from a bat', image: '/batwings.png' },
+  {
+    name: 'Dragon Scales',
+    description: 'Scales harvested from a magical dragon',
+    image: './dragonscale.png',
+  },
+  {
+    name: 'Jellybeans',
+    description: 'Some jellybeans from a random gas station',
+    image: './jellybean.png',
+  },
 ]
-
 </script>
 
 <style scoped>
-*{
-    font-family: "inter";
-    text-align: center ;
+* {
+  font-family: 'inter';
+  text-align: center;
+}
+
+h1 {
+    color: black;
+}
+
+.page {
+  min-height: 100vh;
+  background-image: url('/background.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  position: relative;
 }
 
 .cauldron {
-    width: 50%;
-    height: 750px;
-    color: white;
-    background-color: black;
+  position: absolute;
+  bottom: 20px;
+  left: 650px;
 }
 
-img {
-    max-height: 500px;
-    width: auto
+.cauldron img {
+  height: 340px;
+}
+
+.recipelist {
+  position: absolute;
+  right: 100px;
+}
+
+.recipelist img{
+    height: 850px;  
 }
 
 .container {
-    margin: 25px;
-    margin-left: 25px;
-    margin-right: 25px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+  margin: 25px;
+  margin-left: 25px;
+  margin-right: 25px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
-
 </style>
